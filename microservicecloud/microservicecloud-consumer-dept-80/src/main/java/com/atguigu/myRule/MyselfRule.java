0468b52467af997b0896fa77fce5d21b9f4a5a4f
liaosixing <522043032@qq.com>
2020-03-08 20:40:09 +0800

    添加springclouddemo

---
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myRule/MyselfRule.java
@@ -0,0 +1,8 @@
+package com.atguigu.myRule;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyselfRule {
+	
+}
---
+++ microservicecloud/microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/Dept8002_App.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient //开启eureka客户端功能  将该服务注册进eureka服务端
+public class Dept8002_App {
+	public static void main(String[] args) {
+		SpringApplication.run(Dept8002_App.class, args);
+	}
+}
